55f0742fe04a714668381bae6e4ce37d7ed49d95
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-02-23 10:48:47 +0530

    xzsxc

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://mycompany-chi.vercel.app/"
+    origin: "https://mycompany-chi.vercel.app"
 }));
 
 app.use(cors());
